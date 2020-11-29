f9854c0eefb02d0d51396d1c1423c75d0e64dc47
killanmsheen <74465851+killanmsheen@users.noreply.github.com>
2020-11-29 12:12:48 -0800

    Changes to Website

--- resources/js/cmds.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $(".box").on("click", "li", function(){
-        var tabsID = $(this).attr("id");
-        $(this).addClass("active").siblings().removeClass("active");
-        $("#" + tabsID + "-content-box").addClass("active").siblings().removeClass("active");
-    })
-})
